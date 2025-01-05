1c6a51e3fcbffd5d012535a463131c950dbcf160
vistara-dev <dev@vistara.dev>
2025-01-05 12:21:22 +0000

    crew generation from YAML

--- src/client/client.py
+++ src/client/client.py
@@ -6,6 +6,14 @@ from ..common.types import (
     MessageType,
 )
 from dataclasses import asdict
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+import yaml
+
+
+def read_yaml(config):
+    with open(config, "r") as file:
+        return yaml.safe_load(file)
 
 
 def make_request(websocket, request_type, request):
@@ -18,24 +26,67 @@ def make_request(websocket, request_type, request):
     return resp_type(**json.loads(websocket.recv()))
 
 
-def main():
-    with connect("ws://localhost:8000") as websocket:
+def create_virtual_tools(websocket, tools: list[str]):
+    virtual_tools = []
+    for tool in tools:
         tool = make_request(
             websocket,
             MessageType.AGENT_METADATA,
-            AgentMetadataRequest(tool_id="TOOL_ID"),
+            AgentMetadataRequest(tool_id=tool),
         )
-        print(f"Received {tool}")
+        virtual_tools.append(
+            create_virtual_tool(
+                tool,
+                lambda tool, kwargs: make_request(
+                    websocket,
+                    MessageType.AGENT_EXECUTE,
+                    AgentExecuteRequest(tool=tool, kwargs=kwargs),
+                ).response,
+            )
+        )
 
-        virtual_tool = create_virtual_tool(
-            tool,
-            lambda tool, kwargs: make_request(
-                websocket,
-                MessageType.AGENT_EXECUTE,
-                AgentExecutionRequest(tool=tool, kwargs=kwargs),
-            ).response,
-        )
-        print(virtual_tool)
+
+def generate_crew(websocket):
+    class GeneratedCrew:
+        @crew
+        def crew(self) -> Crew:
+            return Crew(
+                agents=self.agents,
+                tasks=self.tasks,
+                process=Process.sequential,
+                verbose=True,
+            )
+
+    agents_config = read_yaml("config/agents.yaml")
+    tasks_config = read_yaml("config/tasks.yaml")
+
+    for agent_name, config in agents_config.values():
+
+        @agent
+        def fn(self) -> Agent:
+            return Agent(
+                config=self.agents_config[agent_name],
+                verbose=True,
+                tools=create_virtual_tools(config.get("tools", [])),
+            )
+
+        setattr(GeneratedCrew, agent_name, fn)
+
+    for task_name in tasks_config:
+
+        @task
+        def fn(self) -> Task:
+            return Task(config=self.tasks_config[task_name])
+
+        setattr(GeneratedCrew, task_name, fn)
+
+    return CrewBase(GeneratedCrew)()
+
+
+def main():
+    with connect("ws://localhost:8000") as websocket:
+        generated_crew = generate_crew(websocket)
+        generated_crew.crew().kickoff(inputs={"topic": "AI Agents"})
 
 
 main()
