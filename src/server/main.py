554c42272f02f593b260ce83b77f2b5015c9d872
vistara-dev <dev@vistara.dev>
2025-01-26 10:51:21 +0000

    add solana tool

--- src/server/main.py
+++ src/server/main.py
@@ -4,6 +4,7 @@ from qdrant_client import QdrantClient
 
 from .api import create_api
 from .registry import Registry
+from ..tools.agentipy_tools import gen_tools
 
 
 def main():
@@ -14,6 +15,8 @@ def main():
     # TODO this will be made dynamic in the future where tools will be
     # run as their own services
     registry.register_tool("SerperDevTool", crewai_tools.SerperDevTool())
+    for tool_name, tool in gen_tools():
+        registry.register_tool(tool_name, tool)
 
     uvicorn.run(create_api(registry), host="0.0.0.0", port=8000)
 
---
+++ src/tools/agentipy_tools.py
@@ -0,0 +1,78 @@
+import asyncio
+import agentipy
+import os
+from crewai import Agent, Crew, Task
+from crewai.tools import BaseTool
+import inspect
+import typing
+from pydantic import Field, BaseModel, create_model
+
+
+# Check whether the argument is marked as optional with the
+# typing.Optional hint
+def is_optional_arg(annotation):
+    return isinstance(annotation, typing._GenericAlias) and (
+        annotation.__origin__ is Union and type(None) in annotation.__args__
+    )
+
+
+def gen_tool(method_name, method):
+    model_fields = {}
+
+    for arg, arg_type in typing.get_type_hints(method).items():
+        if not (arg_type in (bool, str, int, float, type(None))):
+            if is_optional_arg(arg_type):
+                continue
+
+            raise ValueError(
+                f"arg {arg} of {method_name} not of primitive type: {arg_type}"
+            )
+
+        model_fields[arg] = (
+            arg_type,
+            Field(..., description=arg.replace("_", " ").title()),
+        )
+
+    class Tool(BaseTool):
+        name: str = method_name
+        description: str = (
+            inspect.getdoc(method) or f"Invoke {method_name} method."
+        )
+        args_schema: typing.Type[BaseModel] = create_model(
+            method_name.replace("_", " ").title(), **model_fields
+        )
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def _run(self, **kwargs):
+            print(f"Invoked {method_name}: {kwargs}")
+            return asyncio.run(method(**kwargs))
+
+    return Tool()
+
+
+def gen_tools():
+    agent = agentipy.SolanaAgentKit(rpc_url="https://api.devnet.solana.com")
+    tools = []
+
+    methods = inspect.getmembers(agent, predicate=inspect.iscoroutinefunction)
+    for method_name, method in methods:
+        if method_name.startswith("_"):
+            continue
+        if method_name not in {
+            "fetch_price",
+            "get_tps",
+            "stake",
+            "get_address_name",
+        }:
+            continue
+
+        tools.append(
+            (
+                f"Solana {method_name.replace('_', ' ').title()}",
+                gen_tool(method_name, method),
+            )
+        )
+
+    return tools
